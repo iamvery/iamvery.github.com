9054a132aa3bc9596ec42db229e593c5142f8a57
Jay Hayes <ur@iamvery.com>
2012-11-28 11:42:13 -0600

    Added example site to get started

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'jekyll',        '~> 0.11'
+gem 'sass',          '~> 3.2'
+gem 'coffee-script', '~> 2.2'
---
+++ _plugins/scss_converter.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /scss/i
+    end
+
+    def output_ext(ext)
+      '.css'
+    end
+
+    def convert(content)
+      begin
+        engine = Sass::Engine.new content, :syntax => :scss, :load_paths => ['./css/'], :style => :compressed
+        engine.render
+      rescue StandardError => e
+        puts "Sass Error: #{e.message}"
+      end
+    end
+  end
+end
+
